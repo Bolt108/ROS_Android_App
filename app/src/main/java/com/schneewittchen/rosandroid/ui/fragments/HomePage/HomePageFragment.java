85cf6a77e5cded13907cad720db2c0c0f5db073d
ryanzhang15 <ryanjzhang@gmail.com>
2021-07-21 15:44:12 +0800

    Add joystick by accident.
    for ewar

---
+++ app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/HomePage/HomePageFragment.java
@@ -0,0 +1,38 @@
+package com.schneewittchen.rosandroid.ui.fragments.HomePage;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.schneewittchen.rosandroid.R;
+
+public class HomePageFragment extends Fragment {
+    
+    public static final String TAG = HomePageFragment.class.getSimpleName();
+    
+    public static HomePageFragment newInstance() {return new HomePageFragment();}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated: I WAS CREATED LITTLE BITCH.");
+    }
+}

ae712be95246ea15e6778824b2a647f42f298826
gen-e-sis <67941300+gen-e-sis@users.noreply.github.com>
2021-07-30 16:10:57 +0800

    Added working switch
    Switch is working

--- app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/settings/SettingsFragment.java
@@ -1,13 +1,18 @@
 package com.schneewittchen.rosandroid.ui.fragments.settings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -19,6 +24,8 @@ import org.jetbrains.annotations.NotNull;
 import com.schneewittchen.rosandroid.ui.general.TabButton;
 
 public class SettingsFragment extends Fragment {
+
+    private final static String TAG = SettingsFragment.class.getSimpleName();
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -32,6 +39,19 @@ public class SettingsFragment extends Fragment {
         TabButton masterButton = new TabButton(v.findViewById(R.id.master_button));
         masterButton.linkToFragment(3, getParentFragmentManager().beginTransaction());
 
+        SwitchCompat sw = v.findViewById(R.id.switch1);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    Log.d(TAG, "onCheckChanged: Johnny gets minimum wage :)");
+                } else {
+                    // The toggle is disabled
+                    Log.d(TAG, "onCheckChanged: Johnny gets minimum wage :(");
+                }
+            }
+        });
+
         return v;
     }
 
@@ -39,5 +59,7 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+
 }
 
